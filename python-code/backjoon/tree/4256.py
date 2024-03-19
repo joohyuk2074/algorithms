2d9540f72eb7bf4e0db3647cf56dcea073738fdf
weekbelt4207 <75966556+weekbelt4207@users.noreply.github.com>
2024-03-19 23:26:41 +0900

    2024.03.19 LIS 학습ㅈ

---
+++ python-code/backjoon/tree/4256.py
@@ -0,0 +1,10 @@
+class Tree:
+    def __init__(self, value, left, right):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
